b7d4afa7ce4c28c1a20ad413c1ab7294ca24d3ad
himanshu-grg <gargh600@gmail.com>
2022-06-06 22:20:27 +0530

    m

---
+++ src/components/navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
